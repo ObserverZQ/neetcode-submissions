7a57cf9a1d6098a3c497a2bc41f985531dfaada5
ObserverZQ <657282348@qq.com>
2026-04-23 17:40:07 +0800

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    # iteration. for every element, we add it to existing subsets to create a bunch of new subets and add them to the overall result
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for num in nums:
+            # original_subsets = 
+            subsets = list(map(lambda x: x+[num], res))
+            res += subsets
+            # neetcode code:
+            # res += [subset + [num] for subset in res]
+        return res
