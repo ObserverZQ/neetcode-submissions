10a13f647b1a932d37fe2e208dd86ef647de4154
ObserverZQ <657282348@qq.com>
2026-04-25 02:12:07 +0800

    Add: binary-tree-right-side-view - submission-6

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-6.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    # dfs recursive solution. looking at right first then left,
+    # in every depth we add the first node we see into the res list.
+    # time O(n), space O(n)
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(node: Optional[TreeNode], depth: int) -> None:
+            if not node:
+                return None
+            # when we reach next level, we have exactly len elements in the res
+            if len(res) == depth: # say on depth 1, before adding, we have 1 element
+                res.append(node.val)
+            dfs(node.right, depth + 1) # calling right first make sure we always get right first if right is not None
+            dfs(node.left, depth + 1) # otherwise we can view the left from right
+        dfs(root, 0)
+        return res
