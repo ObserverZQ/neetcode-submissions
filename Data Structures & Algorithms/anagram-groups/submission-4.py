5cc4467093ff3ec064d592186702a8e6cd7e3c07
ObserverZQ <657282348@qq.com>
2026-04-26 21:04:43 +0800

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    # my solution: create a helper funciton to generate uni presentations for
+    # anagram strings. sort the array of unicodes then convert them to string format.
+    # in the main function, we store a table of uni -> str list
+    # finally we print the values and use list comprehension to form the answer.
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        table = {}
+        for i in range(len(strs)):
+            uni = self.getUni(strs[i])
+            if uni in table:
+                table[uni].append(strs[i])
+            else:
+                table[uni] = [strs[i]]
+        return [val for val in table.values()] # list(table.values())
+
+    def getUni(self, s: str) -> bool:
+        base = ord('a')
+        res = []
+        for c in s:
+            res.append(ord(c) - base)
+        return str(sorted(res))
