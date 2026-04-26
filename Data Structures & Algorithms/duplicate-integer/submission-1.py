4fd85ab3075293b1c806e45cf6533c8d7512f527
ObserverZQ <657282348@qq.com>
2026-04-26 19:10:04 +0800

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        nummap = {}
+        for num in nums:
+            if num in nummap:
+                return True
+            else:
+                nummap[num] = True
+        return False
