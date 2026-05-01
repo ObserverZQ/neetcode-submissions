f079cca503bea6cf4df0db3c7ea3313ea75260ed
ObserverZQ <657282348@qq.com>
2026-05-02 03:46:06 +0800

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        # create a tuple list with elements from two lists with corresponding indices
+        pair = [(p, s) for p, s in zip(position, speed)]
+        # print(f'pair: {pair}')
+        pair.sort(reverse = True) # sort the tuple list with the position in descending order
+        # print(f'pair: {pair}')
+        stack = []
+        for p, s in pair:
+            stack.append((target - p) / s) # the time it takes for the current car to reach the destination
+            if len(stack) >= 2 and stack[-1] <= stack[-2]: # the front car(-2) takes longer, so the later car catches up and merges with it
+                stack.pop()
+        return len(stack)
