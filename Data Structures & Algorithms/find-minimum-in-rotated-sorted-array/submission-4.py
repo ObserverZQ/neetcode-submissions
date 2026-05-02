92392fbdf9d3e3650b7f63dc852fb8797fa06a6b
ObserverZQ <657282348@qq.com>
2026-05-03 01:03:04 +0800

    Add: find-minimum-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    # lower bound
+    # In a rotated sorted array, the minimum element is the first element of the rotated portion.
+    def findMin(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            m = l + (r - l) // 2
+            if nums[m] < nums[r]:
+                r = m
+            else:
+                l = m + 1
+        return nums[l]
