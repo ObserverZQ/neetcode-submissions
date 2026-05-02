ac63ebf01b445d70d28e6b587e710f7ffb2779b8
ObserverZQ <657282348@qq.com>
2026-05-02 23:46:08 +0800

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    # staircase search, time O(m + n), space O(1)
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        r, c = 0, n - 1
+        while r < m and c >= 0:
+            if matrix[r][c] > target:
+                c -= 1
+            elif matrix[r][c] < target:
+                r += 1
+            else:
+                return True
+        return False
