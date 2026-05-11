3647d49241a347c1dc2406a4c78b76823042de38
ObserverZQ <657282348@qq.com>
2026-05-11 17:06:22 +0800

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    # for recursive solution,
+    # we use a set to record calculated steps for smaller n to accelerate
+    def climbStairs(self, n: int) -> int:
+        stepMap = {}
+        def steps(n: int) -> int:
+            if n<= 1:
+                return 1
+            elif n in stepMap:
+                return stepMap[n]
+            else:
+                stepMap[n] = steps(n - 1) + steps(n - 2)
+                return stepMap[n]
+        return steps(n)
