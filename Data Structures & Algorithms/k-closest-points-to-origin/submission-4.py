12c38f56d47f369c7a488afc85078a8f1dfc251c
ObserverZQ <657282348@qq.com>
2026-05-20 21:27:14 +0800

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.py
@@ -0,0 +1,32 @@
+class Solution:
+    '''
+    If the pivot ends up at index p:
+    If p == k, then the left side already contains the k closest points.
+    If p < k, search the right half.
+    If p > k, search the left half.
+    Time complexity: O(n) in average case, O(n2) in worst case.
+    Space complexity: O(1)
+    '''
+    def kClosest(self, points, k):
+        euclidean = lambda x: x[0] ** 2 + x[1] ** 2
+        def partition(l, r):
+            pivotIdx = r
+            pivotDist = euclidean(points[pivotIdx])
+            i = l
+            for j in range(l, r):
+                if euclidean(points[j]) <= pivotDist:
+                    points[i], points[j] = points[j], points[i]
+                    i += 1
+            points[i], points[r] = points[r], points[i]
+            return i
+
+        L, R = 0, len(points) - 1
+        pivot = len(points)
+
+        while pivot != k:
+            pivot = partition(L, R)
+            if pivot < k:
+                L = pivot + 1
+            else:
+                R = pivot - 1
+        return points[:k]
