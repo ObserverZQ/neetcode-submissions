f1c8ef649e52c245d8c308573f02976de0df1a40
ObserverZQ <657282348@qq.com>
2026-05-21 19:53:59 +0800

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.py
@@ -0,0 +1,20 @@
+class LRUCache:
+    # using the builtin ordermap, we can use move_to_end to put the most recently
+    # used key to the end of the map. so the least recently used will be at the front
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key) # here last=True by default
+        return self.cache[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
+     
