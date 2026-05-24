2d0c7874822aed54171331048d35ae048e5bec71
ObserverZQ <657282348@qq.com>
2026-05-24 19:05:26 +0800

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        original_color = image[sr][sc]
+        m = len(image)
+        n = len(image[0])
+
+        '''
+        a good edge case:
+        if orig == color:
+            return image
+        '''
+        def dfs(r: int, c: int, visited: object) -> None:
+            # check r and c legality
+            if r < 0 or c < 0 or r >= m or c >= n or (r, c) in visited:
+                return
+            # check color
+            if image[r][c] != original_color:
+                return
+            
+            image[r][c] = color
+            visited.add((r, c))
+
+            dfs(r + 1, c, visited)
+            dfs(r - 1, c, visited)
+            dfs(r, c + 1, visited)
+            dfs(r, c - 1, visited)
+
+            # we may not have to remove (r, c) from visited
+
+        dfs(sr, sc, set())
+        return image
