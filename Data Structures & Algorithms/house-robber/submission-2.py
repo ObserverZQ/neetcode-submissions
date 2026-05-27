21c071dc751916ca762cc6d01cebae4846d5837b
ObserverZQ <657282348@qq.com>
2026-05-27 16:27:07 +0800

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    # dp. time O(n), space O(n). without memo the time would be O(2 to the nth power)
+    def rob(self, nums: List[int]) -> int:
+        memo = [-1] * len(nums) # save the maximum $ from house i
+
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if memo[i] != -1:
+                return memo[i]
+            # choose between taking the next house or the current house and the next's next house
+            memo[i] = max(dfs(i+1), nums[i] + dfs(i+2))
+            return memo[i]
+        return dfs(0)
