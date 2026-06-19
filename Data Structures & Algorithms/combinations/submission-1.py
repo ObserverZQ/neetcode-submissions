e28113c5503379d0e9ced1e5a1f904cfa4e943c0
ObserverZQ <657282348@qq.com>
2026-06-19 11:40:23 +0800

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    # Time: O(k * 2^n)
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        # i: current number, 1 to n
+        # curArr: the list accumulated so far
+        def helper(i: int, curArr: List[int]) -> None:
+            if len(curArr) == k:
+                res.append(curArr.copy())
+                return
+            if i > n:
+                return
+            curArr.append(i)
+            helper(i + 1, curArr)
+            curArr.pop()
+            helper(i + 1, curArr)
+
+        helper(1, [])
+        return res
