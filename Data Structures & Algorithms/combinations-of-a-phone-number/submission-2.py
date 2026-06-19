53646c3c98d99d2d296285ecb15cbfe7c59d9030
ObserverZQ <657282348@qq.com>
2026-06-19 12:10:14 +0800

    Add: combinations-of-a-phone-number - submission-2

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        # n = 3 or 4, k = 1 for each digit in digits
+        res = []
+        n = 9
+        num_chac = {
+            2: ['a', 'b', 'c'],
+            3: ['d', 'e', 'f'],
+            4: ['g', 'h', 'i'],
+            5: ['j', 'k', 'l'],
+            6: ['m', 'n', 'o'],
+            7: ['p', 'q', 'r', 's'],
+            8: ['t', 'u', 'v'],
+            9: ['w', 'x', 'y', 'z']
+        }
+
+        def helper(curStr, index):
+            if len(curStr) == len(digits) and curStr:
+                res.append(curStr)
+                return
+            if index >= len(digits):
+                return
+            for j in range(index, len(digits)):
+                chacs = num_chac[int(digits[j])]
+                for c in chacs:
+                    curStr += c
+                    helper(curStr, j + 1)
+                    curStr = curStr[:-1]
+
+        helper('', 0)
+        return res
