0d6910a8a983ef15d0bd7c6f63a67ca16df7c4d2
ObserverZQ <657282348@qq.com>
2026-07-01 01:20:25 +0800

    Add: find-first-and-last-position-of-element-in-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-first-and-last-position-of-element-in-sorted-array/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    '''
+    The insertion point for target gives us the first occurrence (if it exists). 
+    '''
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        def binarySearch(target):
+            l, r = 0, n
+            while l < r:
+                m = l + (r - l) // 2
+                if nums[m] >= target:
+                    r = m
+                else:
+                    l = m + 1
+            return l
+
+        start = binarySearch(target)
+        if start == n or nums[start] != target:
+            return [-1, -1]
+
+        return [start, binarySearch(target + 1) - 1]
