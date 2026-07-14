5506e3d30c953d5f7275901504993f2f45ac0228
ObserverZQ <657282348@qq.com>
2026-07-15 00:38:09 +0800

    Add: house-robber-ii - submission-2

---
+++ Data Structures & Algorithms/house-robber-ii/submission-2.py
@@ -0,0 +1,20 @@
+'''
+To handle this, split the problem into two linear subproblems:
+
+Rob houses excluding the first house.
+Rob houses excluding the last house.
+'''
+class Solution:
+
+    def rob(self, nums: List[int]) -> int:
+        return max(nums[0], self.helper(nums[1:]),
+                            self.helper(nums[:-1]))
+
+    def helper(self, nums):
+        rob1, rob2 = 0, 0
+
+        for num in nums:
+            newRob = max(rob1 + num, rob2)
+            rob1 = rob2
+            rob2 = newRob
+        return rob2
