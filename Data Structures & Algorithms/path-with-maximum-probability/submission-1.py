5d06d198289a5fa2df7c9de0462b90399e9d792e
ObserverZQ <657282348@qq.com>
2026-07-22 11:36:09 +0800

    Add: path-with-maximum-probability - submission-1

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        # result = prob[i] * prob[j] * ... all with highest cost
+        # so it is an opposite of min cost Dijkstra
+
+        # 1. create the neighbors hashmap
+        adj = {}
+        for i in range(n): # 0-indexed so we iterate from 0 - 2
+            adj[i] = []
+        # print(zip(edges, succProb))
+        for edge, p in zip(edges, succProb):
+            s, e = edge
+            adj[s].append((e, p))
+            adj[e].append((s, p))
+
+        maxHeap = [(-1, start_node)] # store the total probability of reaching each node in the graph
+        visited = set()
+
+        while maxHeap:
+            p, node = heapq.heappop(maxHeap) # always pop the highest value in the heap
+            visited.add(node)
+
+            if node == end_node:
+                return -p
+            
+            for nei, prob in adj[node]:
+                if nei not in visited:
+                    heapq.heappush(maxHeap, (p * prob, nei))
+        
+        return 0
